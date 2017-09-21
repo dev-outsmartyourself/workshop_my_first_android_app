6c0f3418b555f326e1ac5d1aa77c300ef1f9edb4
Lucas Mafra <lucas.mafra.barros@usp.br>
2017-09-21 01:11:01 -0300

    done

---
+++ app/src/main/java/com/outsmart/starwarsfilms/Activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.outsmart.starwarsfilms.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.outsmart.starwarsfilms.R;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
