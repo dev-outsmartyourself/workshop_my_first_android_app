8ca6cab955c3c390e99ac9c6a456c4dd0cb32797
Lucas Mafra <lucas.mafra.barros@usp.br>
2017-09-21 00:38:21 -0300

    done

--- app/src/main/java/com/outsmart/starwarsfilms/Activities/FilmActivity.java
+++ app/src/main/java/com/outsmart/starwarsfilms/Activities/FilmActivity.java
@@ -5,14 +5,26 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.outsmart.starwarsfilms.DataSources.MockDataSource;
+import com.outsmart.starwarsfilms.Models.FilmModel;
 import com.outsmart.starwarsfilms.R;
+import com.outsmart.starwarsfilms.Utils.DownloadImageTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilmActivity extends Activity  {
+
+    private MockDataSource dataSource = MockDataSource.getInstance();
+    private List<FilmModel> films = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.film_card);
+
+        this.films = dataSource.getAllFilms();
+        FilmModel film = this.films.get(0);
 
         TextView title = (TextView) findViewById(R.id.episode_title);
         ImageView image = (ImageView) findViewById(R.id.episode_image);
@@ -20,11 +32,12 @@ public class FilmActivity extends Activity  {
         TextView episodeNumber = (TextView) findViewById(R.id.episode_number);
         TextView imdbRate = (TextView) findViewById(R.id.episode_imdb);
 
-        title.setText("The Phantom Menace");
-        image.setImageResource(R.drawable.episode_1);
-        year.setText("1999");
-        episodeNumber.setText("Episode 1");
-        imdbRate.setText("6.5/10");
+        DownloadImageTask task = new DownloadImageTask(image);
+        task.execute(film.getImage());
+        title.setText(film.getTitle());
+        year.setText(String.valueOf(film.getEpisodeYear()));
+        episodeNumber.setText("Episode " + film.getEpisodeNumber());
+        imdbRate.setText(film.getImdbRate() + "/10");
     }
 
 }
